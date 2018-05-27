3b1e268eaaef4a4503614f4af461c1a6a17c301b
Recai Oktaş <roktas@gmail.com>
2018-05-27 20:25:21 +0300

    Minimal medley'i düzenle

--- srv/scripts/minimal.sh
+++ srv/scripts/minimal.sh
@@ -8,7 +8,20 @@ source <(curl -fsSL https://she.alaturka.io/source) -boot
 enter github.com/omu/debian/lib/scripts
 	paths ../../bin
 
-	try _/runtime
+	enter ./base
+		try common
+		try operator
+		try font
+		try keyboard
+		try timezone
+		try standard
+		try tweak
+	leave
+
+	enter ./runtime
+		try common
+		try ruby
+	leave
 
 	enter ./virtual
 		! is vagrantable || try vagrant
@@ -17,7 +30,17 @@ enter github.com/omu/debian/lib/scripts
 		! is vmware      || try vmware
 	leave
 
+	enter ./operator
+		try dist
+		try zsh
+		try vim
+		try mc
+		try tmux
+	leave
+
+	try server/common
 	try base/clean
+	try server/clean
 
 	! is vm || try virtual/minimize
 leave
