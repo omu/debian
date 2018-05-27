6051089d3f77d537fa0beb49d165a54a633f8b91
Recai Oktaş <roktas@gmail.com>
2018-05-27 22:37:44 +0300

    base/locale: Debian ve Ubuntu yapılandırmalarını ayır

--- lib/scripts/base/locale.sh
+++ lib/scripts/base/locale.sh
@@ -8,17 +8,25 @@ export DEBIAN_FRONTEND=noninteractive
 
 locale=${locale:-en_US.UTF-8}
 
+if ! echo ':en_US.UTF-8:tr_TR.UTF-8:' | grep -Eq ":$locale:"; then
+	echo >&2 "Unsupported default locale: $locale"
+	exit 1
+fi
+
+# shellcheck disable=SC1091
+distribution=$(unset ID && . /etc/os-release 2>/dev/null && echo "$ID")
+
 apt-get -y --no-install-recommends install locales
 
-debconf-set-selections <<-EOF
-	locales locales/locales_to_be_generated multiselect tr_TR.UTF-8 UTF-8, en_US.UTF-8 UTF-8
-	locales locales/default_environment_locale select $locale
-EOF
+if [[ $distribution = debian ]]; then
+	debconf-set-selections <<-EOF
+		locales locales/locales_to_be_generated multiselect tr_TR.UTF-8 UTF-8, en_US.UTF-8 UTF-8
+		locales locales/default_environment_locale select $locale
+	EOF
 
-if [[ -f /etc/locale.gen ]]; then
 	rm -f /etc/locale.gen
 	dpkg-reconfigure -f noninteractive locales
-else
+elif [[ $distribution = ubuntu ]]; then
 	locale-gen tr_TR.UTF-8 en_US.UTF-8
 fi
 
