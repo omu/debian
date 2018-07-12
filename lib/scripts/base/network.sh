3119a79ee2554346ff288649c5018f17d458ff92
Recai Oktaş <roktas@gmail.com>
2018-07-12 19:23:22 +0300

    base/network: Ubuntu netplan desteği ekle

--- lib/scripts/base/network.sh
+++ lib/scripts/base/network.sh
@@ -4,37 +4,67 @@
 
 set -euo pipefail; [[ -z ${TRACE:-} ]] || set -x
 
-# Prefer networkd over legacy ifupdown for simple cases
-
-# DHCP configured ethernet interface through ifupdown?
-if [[ ! -f /etc/network/interfaces ]]; then
-	echo >&2 'No ifupdown detected; skipping'
+skip() {
+	[[ $# -ne 0 ]] || echo >&2 "$@"
 	exit 0
+}
+
+[[ -n "$(dpkg-query -W -f='${Installed-Size}' systemd 2>/dev/null ||:)" ]] ||
+	skip 'No systemd detected; skipping network setup'
+
+[[ -z "$(dpkg-query -W -f='${Installed-Size}' network-manager 2>/dev/null ||:)" ]] ||
+	skip 'Network Manager detected; skipping network setup'
+
+# shellcheck disable=1091
+distribution=$(unset ID && . /etc/os-release 2>/dev/null && echo "$ID")
+
+if [[ $distribution = debian ]]; then
+	# Prefer networkd over legacy ifupdown for simple cases
+
+	# DHCP configured ethernet interface through ifupdown?
+	[[ -f /etc/network/interfaces ]] ||
+		skip 'No ifupdown detected; skipping network setup'
+
+	grep -Eq '^\s*iface\s+e\w+\s+inet\s+dhcp\b' /etc/network/interfaces ||
+		skip 'Network interface is not suitable for migration; skipping network setup'
+
+	systemctl enable systemd-networkd.service
+	systemctl enable systemd-resolved.service
+
+	rm -f /etc/resolv.conf
+	ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
+
+	# Ensure we have a DHCP configured interface regardless of the interface name
+	cat >/etc/systemd/network/99-dhcp.network <<-EOF
+		[Match]
+		Name=e*
+
+		[Network]
+		DHCP=yes
+	EOF
+	chmod a+r /etc/systemd/network/99-dhcp.network
+
+	systemctl stop networking.service
+
+	systemctl start systemd-networkd.service
+	systemctl start systemd-resolved.service
+
+	rm -f /etc/network/interfaces
+	systemctl disable networking.service
+elif [[ $distribution = ubuntu ]]; then
+	[[ -d /etc/netplan ]] || skip 'No netplan detected; skipping network setup'
+
+	# Ensure we have a DHCP configured interface regardless of the interface name
+	cat >/etc/netplan/99-dhcp.yaml <<-EOF
+		Network:
+		  version: 2
+		  renderer: networkd
+		  ethernets:
+		    eth0:
+		      match:
+		        name: e*
+		      dhcp4: yes
+	EOF
+
+	netplan apply
 fi
-if ! grep -Eq '^\s*iface\s+e\w+\s+inet\s+dhcp\b' /etc/network/interfaces; then
-	echo >&2 'Network interface is not suitable for migration; skipping'
-	exit 0
-fi
-
-systemctl enable systemd-networkd.service
-systemctl enable systemd-resolved.service
-
-rm -f /etc/resolv.conf
-ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
-
-cat >/etc/systemd/network/99-dhcp.network <<-EOF
-	[Match]
-	Name=e*
-
-	[Network]
-	DHCP=yes
-EOF
-chmod a+r /etc/systemd/network/99-dhcp.network
-
-systemctl stop networking.service
-
-systemctl start systemd-networkd.service
-systemctl start systemd-resolved.service
-
-rm -f /etc/network/interfaces
-systemctl disable networking.service
--- lib/scripts/base/network_test.sh
+++ lib/scripts/base/network_test.sh
@@ -2,8 +2,27 @@
 
 set -euo pipefail; [[ -z ${TRACE:-} ]] || set -x
 
-# DHCP configured ethernet interface through ifupdown?
-if [[ -f /etc/network/interfaces ]] && grep -Eq '^\s*iface\s+e\w+\s+inet\s+dhcp\b' /etc/network/interfaces; then
+# shellcheck disable=2120
+skip() {
+	[[ $# -ne 0 ]] || echo >&2 "$@"
+	exit 0
+}
+
+# shellcheck disable=2119
+[[ -n "$(dpkg-query -W -f='${Installed-Size}' systemd 2>/dev/null ||:)" ]] || skip
+
+# shellcheck disable=2119
+[[ -z "$(dpkg-query -W -f='${Installed-Size}' network-manager 2>/dev/null ||:)" ]] || skip
+
+# shellcheck disable=SC1091
+distribution=$(unset ID && . /etc/os-release 2>/dev/null && echo "$ID")
+
+if [[ $distribution = debian ]]; then
+	# shellcheck disable=2119
+	[[ -f /etc/network/interfaces ]] || skip
+	# shellcheck disable=2119
+	! grep -Eq '^\s*iface\s+e\w+\s+inet\s+dhcp\b' /etc/network/interfaces || skip
+
 	goss -g - validate --format documentation <<-EOF
 		service:
 		  systemd-networkd:
@@ -15,6 +34,24 @@ if [[ -f /etc/network/interfaces ]] && grep -Eq '^\s*iface\s+e\w+\s+inet\s+dhcp\
 		  networking:
 		    enabled: false
 		    running: false
+		command:
+		  networkctl --no-legend --no-pager:
+		    exit-status: 0
+		    stdout:
+		      - /e.+\\s+ether\\s+routable\\s+configured/
+		dns:
+		  A:example.com:
+		    resolvable: true
+	EOF
+elif [[ $distribution = ubuntu ]]; then
+	# shellcheck disable=2119
+	[[ -d /etc/netplan ]] || skip
+
+	goss -g - validate --format documentation <<-EOF
+		service:
+		  systemd-networkd:
+		    enabled: true
+		    running: true
 		command:
 		  networkctl --no-legend --no-pager:
 		    exit-status: 0
