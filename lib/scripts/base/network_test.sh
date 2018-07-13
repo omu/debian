a54c519c8d1053b380cb3608e94a611b2abe2e54
Recai Oktaş <roktas@gmail.com>
2018-07-13 11:43:54 +0300

    base/network_test: Yarış durumunu önlemek için bekleme ekle

--- lib/scripts/base/network_test.sh
+++ lib/scripts/base/network_test.sh
@@ -8,6 +8,11 @@ skip() {
 	exit 0
 }
 
+die() {
+	echo >&2 "$@"
+	exit 1
+}
+
 # shellcheck disable=2119
 [[ -n "$(dpkg-query -W -f='${Installed-Size}' systemd 2>/dev/null ||:)" ]] || skip
 
@@ -17,48 +22,57 @@ skip() {
 # shellcheck disable=1091
 distribution=$(unset ID && . /etc/os-release 2>/dev/null && echo "$ID")
 
-if [[ $distribution = debian ]]; then
-	# shellcheck disable=2119
-	[[ -f /etc/network/interfaces ]] || skip
-	# shellcheck disable=2119
-	! grep -Eq '^\s*iface\s+e\w+\s+inet\s+dhcp\b' /etc/network/interfaces || skip
+try=3
+while :; do
+	failed=
+	if [[ $distribution = debian ]]; then
+		# shellcheck disable=2119
+		[[ -f /etc/network/interfaces ]] || skip
+		# shellcheck disable=2119
+		! grep -Eq '^\s*iface\s+e\w+\s+inet\s+dhcp\b' /etc/network/interfaces || skip
 
-	goss -g - validate --format documentation <<-EOF
-		service:
-		  systemd-networkd:
-		    enabled: true
-		    running: true
-		  systemd-resolved:
-		    enabled: true
-		    running: true
-		  networking:
-		    enabled: false
-		    running: false
-		command:
-		  networkctl --no-legend --no-pager:
-		    exit-status: 0
-		    stdout:
-		      - /e.+\\s+ether\\s+routable\\s+configured/
-		dns:
-		  A:example.com:
-		    resolvable: true
-	EOF
-elif [[ $distribution = ubuntu ]]; then
-	# shellcheck disable=2119
-	[[ -d /etc/netplan ]] || skip
+		goss -g - validate --format documentation <<-EOF
+			service:
+			  systemd-networkd:
+			    enabled: true
+			    running: true
+			  systemd-resolved:
+			    enabled: true
+			    running: true
+			  networking:
+			    enabled: false
+			    running: false
+			command:
+			  networkctl --no-legend --no-pager:
+			    exit-status: 0
+			    stdout:
+			      - /e.+\\s+ether\\s+routable\\s+configured/
+			dns:
+			  A:example.com:
+			    resolvable: true
+		EOF
+	elif [[ $distribution = ubuntu ]]; then
+		# shellcheck disable=2119
+		[[ -d /etc/netplan ]] || skip
 
-	goss -g - validate --format documentation <<-EOF
-		service:
-		  systemd-networkd:
-		    enabled: true
-		    running: true
-		command:
-		  networkctl --no-legend --no-pager:
-		    exit-status: 0
-		    stdout:
-		      - /e.+\\s+ether\\s+routable\\s+configured/
-		dns:
-		  A:example.com:
-		    resolvable: true
-	EOF
-fi
+		goss -g - validate --format documentation <<-EOF
+			service:
+			  systemd-networkd:
+			    enabled: true
+			    running: true
+			command:
+			  networkctl --no-legend --no-pager:
+			    exit-status: 0
+			    stdout:
+			      - /e.+\\s+ether\\s+routable\\s+configured/
+			dns:
+			  A:example.com:
+			    resolvable: true
+		EOF
+	fi || failed=true
+
+	[[ -n $failed ]] || break
+	((--try >  0)) || die "Network setup test failed"
+
+	sleep 1
+done
