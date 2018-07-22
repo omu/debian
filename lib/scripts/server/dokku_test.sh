93f343d317f27c0f8518fbb9808c520936e235c7
Recai Oktaş <roktas@gmail.com>
2018-07-22 17:56:58 +0300

    server/dokku: Test ekle

--- lib/scripts/server/dokku_test.sh
+++ lib/scripts/server/dokku_test.sh
@@ -2,4 +2,25 @@
 
 set -euo pipefail; [[ -z ${TRACE:-} ]] || set -x
 
-# TODO
+dokku version
+dokku domains:report | grep -Eq "vhosts:\s+$(hostname)[.]local"
+dokku plugin:list | grep -Eq '^\s+(postgres|redis|letsencrypt)\b'
+
+goss -g - validate --format documentation <<-EOF
+	service:
+	  docker:
+	    enabled: true
+	    running: true
+	group:
+	  docker:
+	    exists: true
+	user:
+	  dokku:
+	    exists: true
+	    groups:
+	    - docker
+	    home: /home/dokku
+	group:
+	  dokku:
+	    exists: true
+EOF
