bdac265523ecfad093f07bae82fbacdef8a4927e
Recai Oktaş <roktas@gmail.com>
2018-07-28 15:49:13 +0300

    server/dokku: Provizyonlama Vagrant odaklı olmamalı
    Dokku'yu Vagrant altında denemek için mutlaka ek ayarlar yapılması
    gerekiyor.  İptal ettiğimiz düzenlemeler yeterli gelmiyor.  Bu konuyu
    tamamen "akıllı" bir öntanımlı Vagrantfile'a bırakalım.  Şu halde en
    makul öntanımlı davranış Vagrantsız senaryolarda yapılandırmayı Dokku
    web config ile tamamlamak.

--- lib/scripts/server/dokku.sh
+++ lib/scripts/server/dokku.sh
@@ -6,28 +6,15 @@ set -euo pipefail; [[ -z ${TRACE:-} ]] || set -x
 
 export DEBIAN_FRONTEND=noninteractive
 
-dokku_domain=${dokku_domain:-localtest.me}
-
 latest=$(
 	curl -fsSL https://api.github.com/repos/dokku/dokku/releases/latest |
 	jq -r .tag_name
 )
 
-keyfile=/tmp/vagrant.pub
-
-# Use Vagrant insecure key
-curl -fsSL https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub >"$keyfile"
-
 curl -fsSL https://raw.githubusercontent.com/dokku/dokku/"$latest"/bootstrap.sh |
 	DOKKU_TAG="$latest" \
-	DOKKU_WEB_CONFIG=false \
-	DOKKU_VHOST_ENABLE=true \
-	DOKKU_HOSTNAME="$dokku_domain" \
-	DOKKU_KEY_FILE="$keyfile" \
 	\
 	bash -s
-
-rm -f "$keyfile"
 
 dokku plugin:install https://github.com/dokku/dokku-postgres.git
 dokku plugin:install https://github.com/dokku/dokku-letsencrypt.git
--- lib/scripts/server/dokku_test.sh
+++ lib/scripts/server/dokku_test.sh
@@ -2,10 +2,7 @@
 
 set -euo pipefail; [[ -z ${TRACE:-} ]] || set -x
 
-dokku_domain=${dokku_domain:-localtest.me}
-
 dokku version
-dokku domains:report | grep -Eq "vhosts:\s+${dokku_domain}"
 dokku plugin:list | grep -Eq '^\s+(postgres|letsencrypt|http-auth|redirect|maintenance)\b'
 
 goss -g - validate --format documentation <<-EOF
