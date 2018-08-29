eeeeeeb7bfcee356a85807a2accd7b3b5f183eed
Recai Oktaş <roktas@gmail.com>
2018-08-29 21:06:24 +0300

    desktop/avahi: LXC'de avahi kurulumu için geçici çözüm uygula

--- lib/scripts/desktop/avahi.sh
+++ lib/scripts/desktop/avahi.sh
@@ -6,6 +6,19 @@ set -euo pipefail; [[ -z ${TRACE:-} ]] || set -x
 
 export DEBIAN_FRONTEND=noninteractive
 
-apt-get install -y --no-install-recommends avahi-daemon libnss-mdns avahi-utils
+if ! apt-get install -y --no-install-recommends avahi-daemon libnss-mdns avahi-utils; then
+	case $(systemd-detect-virt) in
+	lxc)
+		# Workaround for https://github.com/lxc/lxd/issues/2948
+		if [[ -f /etc/avahi/avahi-daemon.conf ]]; then
+			sed -e '/^rlimit-nproc=/ s/^/#/' -i /etc/avahi/avahi-daemon.conf
+			echo >&2 'Workaround for LXC applied'
+		fi
+		systemctl restart avahi-daemon
+		;;
+	*)
+		exit 1
+	esac
+fi
 
 systemctl stop avahi-daemon && systemctl disable avahi-daemon
