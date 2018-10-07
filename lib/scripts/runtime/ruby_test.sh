f7d8f52744d2420082546cf9ce53688776dbf386
Recai Oktaş <roktas@gmail.com>
2018-10-07 18:48:31 +0300

    runtime/ruby: Test düzelt

--- lib/scripts/runtime/ruby_test.sh
+++ lib/scripts/runtime/ruby_test.sh
@@ -4,25 +4,27 @@ set -euo pipefail; [[ -z ${TRACE:-} ]] || set -x
 
 ruby --version
 
-goss -g - validate --format documentation <<-EOF
-	package:
-	  ruby:
-	    installed: true
-	  ruby-dev:
-	    installed: true
-	  rake:
-	    installed: true
-	command:
-	  ruby --version:
-	    exit-status: 0
-	    stdout:
-	    - "/2.[1-9].[0-9]/"
-	    timeout: 10000
-	  which bundler:
-	    exit-status: 0
-	    stdout:
-	    - /usr/local/bin/bundler
-EOF
+if [[ -n ${ruby_use_suites:-} ]]; then
+	goss -g - validate --format documentation <<-EOF
+		command:
+		which ruby:
+		exit-status: 0
+		stdout:
+		- /usr/local/bin/ruby
+		which rake:
+		exit-status: 0
+		stdout:
+		- /usr/local/bin/rake
+		which gem:
+		exit-status: 0
+		stdout:
+		- /usr/local/bin/gem
+		which bundler:
+		exit-status: 0
+		stdout:
+		- /usr/local/bin/bundler
+	EOF
 
-# Confirm jemalloc support
-[[ -z ${ruby_use_jemalloc:-} ]] || ruby -r rbconfig -e "puts RbConfig::CONFIG['LIBS']" | grep -q jemalloc
+	# Confirm jemalloc support
+	ruby -r rbconfig -e "puts RbConfig::CONFIG['LIBS']" | grep -q jemalloc
+fi
