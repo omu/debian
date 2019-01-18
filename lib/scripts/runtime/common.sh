3183e0866011b6447378f4a6086f93c26f5e2fb7
Recai Oktaş <roktas@gmail.com>
2019-01-18 21:11:36 +0300

    runtime/common: wkhtmltopdf paketini ekle

--- lib/scripts/runtime/common.sh
+++ lib/scripts/runtime/common.sh
@@ -70,3 +70,7 @@ apt-get -y install --no-install-recommends \
 	sqlite3 \
 	#
 
+curl -fsLO https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.stretch_amd64.deb
+dpkg -i wkhtmlto* 2>/dev/null || true
+rm -f wkhtmlto*
+apt-get -y install --no-install-recommends --fix-broken
