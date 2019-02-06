3983d6cb335c863934a9154266f8647a4522b728
Recai Oktaş <roktas@gmail.com>
2019-02-06 13:50:42 +0300

    runtime/chrome: Chrome sürücü kurulumu ekle

---
+++ lib/scripts/runtime/chrome.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Install Chrome driver
+
+set -euo pipefail; [[ -z ${TRACE:-} ]] || set -x
+
+export DEBIAN_FRONTEND=noninteractive
+
+curl -fsSL https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo 'deb http://dl.google.com/linux/chrome/deb/ stable main' >>/etc/apt/sources.list.d/chrome.list
+apt-get -y update
+
+apt-get -y install --no-install-recommends google-chrome-stable
+# shellcheck disable=2016
+sed -i 's+"$HERE/chrome"+"$HERE/chrome" --no-sandbox+g' /opt/google/chrome/google-chrome
+
+apt-get -y install --no-install-recommends chromedriver
