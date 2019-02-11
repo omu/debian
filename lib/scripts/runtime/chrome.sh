5b2fcd73042b46efbb7c0df7ae9aba1e948ff9d4
Recai Oktaş <roktas@gmail.com>
2019-02-11 14:08:04 +0300

    Sadece chromedriver kurulması yeterli

--- lib/scripts/runtime/chrome.sh
+++ lib/scripts/runtime/chrome.sh
@@ -6,12 +6,4 @@ set -euo pipefail; [[ -z ${TRACE:-} ]] || set -x
 
 export DEBIAN_FRONTEND=noninteractive
 
-curl -fsSL https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-echo 'deb http://dl.google.com/linux/chrome/deb/ stable main' >>/etc/apt/sources.list.d/chrome.list
-apt-get -y update
-
-apt-get -y install --no-install-recommends google-chrome-stable
-# shellcheck disable=2016
-sed -i 's+"$HERE/chrome"+"$HERE/chrome" --no-sandbox+g' /opt/google/chrome/google-chrome
-
-apt-get -y install --no-install-recommends chromedriver
+apt-get -y update && apt-get -y install --no-install-recommends chromedriver
