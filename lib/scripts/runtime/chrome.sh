0baa18b55ac8aee74b3baad656c5ea91522cd72f
Recai Oktaş <roktas@gmail.com>
2019-02-20 10:27:43 +0300

    runtime/chrome: Chrome üst geliştiriciden kurulabilsin

--- lib/scripts/runtime/chrome.sh
+++ lib/scripts/runtime/chrome.sh
@@ -6,4 +6,14 @@ set -euo pipefail; [[ -z ${TRACE:-} ]] || set -x
 
 export DEBIAN_FRONTEND=noninteractive
 
+if [[ -n ${chrome_install_upstream:-} ]]; then
+	curl -fsSL https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+	echo 'deb http://dl.google.com/linux/chrome/deb/ stable main' >>/etc/apt/sources.list.d/chrome.list
+	apt-get -y update
+
+	apt-get -y install --no-install-recommends google-chrome-stable
+	# shellcheck disable=2016
+	sed -i 's+"$HERE/chrome"+"$HERE/chrome" --no-sandbox+g' /opt/google/chrome/google-chrome
+fi
+
 apt-get -y update && apt-get -y install --no-install-recommends chromedriver
