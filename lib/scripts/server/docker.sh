e8a7c3fa0bd25937a1172f3de7af7d1b9e14e740
Recai Oktaş <roktas@gmail.com>
2019-02-24 13:54:19 +0300

    server/docker: Gereksiz sudo kullanımını kaldır

--- lib/scripts/server/docker.sh
+++ lib/scripts/server/docker.sh
@@ -8,7 +8,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 operator=${operator:-$(id -rnu 1000 2>/dev/null)}
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 
 add-apt-repository \
 	"deb [arch=amd64] https://download.docker.com/linux/debian \
