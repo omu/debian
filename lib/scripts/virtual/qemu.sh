6fc3bd7d44fbf8de9153ff2ad78046a6349c1b8a
Hüseyin Tekinaslan <htkaslan@gmail.com>
2019-03-09 23:49:55 +0300

    Install guest agent for FusionCompute

--- lib/scripts/virtual/qemu.sh
+++ lib/scripts/virtual/qemu.sh
@@ -7,3 +7,19 @@ set -euo pipefail; [[ -z ${TRACE:-} ]] || set -x
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get -y install --no-install-recommends qemu-guest-agent spice-vdagent nfs-common
+
+# Install guest agent for Huawei FusionCompute
+sha256=507181f2cb720d1b8626dbcacd78cd45465f963eba29c2fdd6cf7ce847bc4458
+
+curl -fL --retry 3 -o vmtools.tar.xz https://file.omu.sh/huawei-vmtools.tar.xz
+echo "$sha256 vmtools.tar.xz" | sha256sum -c - || exit 1
+tar -xJf vmtools.tar.xz -C .
+rm -f vmtools.tar.xz
+
+pushd vmtools
+./install
+popd
+
+rm -rf vmtools
+
+systemctl stop vm-agent && systemctl disable vm-agent
