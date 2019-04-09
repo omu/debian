d307b621328e7a4450f56224c0ad66249e1f47df
Recai Oktaş <roktas@gmail.com>
2019-04-09 14:50:14 +0300

    git-secrets'i depodan kur

--- lib/scripts/operator/git.sh
+++ lib/scripts/operator/git.sh
@@ -6,7 +6,15 @@ export DEBIAN_FRONTEND=noninteractive
 
 cd "$(dirname "$0")/../../.."
 
-apt-get -y install --no-install-recommends git git-secrets
+apt-get -y install --no-install-recommends git
+
+curl -fsSL -o /usr/local/bin/git-secrets \
+	https://raw.githubusercontent.com/awslabs/git-secrets/master/git-secrets
+chmod +x /usr/local/bin/git-secrets
+
+mkdir -p /usr/local/share/man/man1
+curl -fsSL -o /usr/local/share/man/man1/git-secrets.1 \
+	https://raw.githubusercontent.com/awslabs/git-secrets/master/git-secrets.1
 
 shopt -s nullglob
 [[ -z $(echo etc/git/*) ]] || cp -a etc/git/* /etc/
