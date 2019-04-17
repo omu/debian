2d0b36cd3aa6b34a4eae30900a504a2dfe4c6e4b
Recai Oktaş <roktas@gmail.com>
2019-04-17 13:20:13 +0300

    operator/git: Git yapılandırmasını geliştir

--- lib/scripts/operator/git.sh
+++ lib/scripts/operator/git.sh
@@ -2,19 +2,35 @@
 
 set -euo pipefail; [[ -z ${TRACE:-} ]] || set -x
 
+git_lfs=${git_lfs:true}
+git_secrets=${git_secrets:true}
+git_tig=${git_tig:-}
+
+shopt -s nullglob
+
 export DEBIAN_FRONTEND=noninteractive
 
 cd "$(dirname "$0")/../../.."
 
 apt-get -y install --no-install-recommends git
 
-curl -fsSL -o /usr/local/bin/git-secrets \
-	https://raw.githubusercontent.com/awslabs/git-secrets/master/git-secrets
-chmod +x /usr/local/bin/git-secrets
+[[ -z ${git_lfs:-} ]] || apt-get -y install --no-install-recommends git-lfs
+[[ -z ${git_tig:-} ]] || apt-get -y install --no-install-recommends tig
 
-mkdir -p /usr/local/share/man/man1
-curl -fsSL -o /usr/local/share/man/man1/git-secrets.1 \
-	https://raw.githubusercontent.com/awslabs/git-secrets/master/git-secrets.1
+if [[ -n ${git_secrets:-} ]]; then
+	curl -fsSL -o /usr/local/bin/git-secrets \
+		https://raw.githubusercontent.com/awslabs/git-secrets/master/git-secrets
+	chmod +x /usr/local/bin/git-secrets
 
-shopt -s nullglob
+	mkdir -p /usr/local/share/man/man1
+	curl -fsSL -o /usr/local/share/man/man1/git-secrets.1 \
+		https://raw.githubusercontent.com/awslabs/git-secrets/master/git-secrets.1
+fi
+
+curl -fsSL -o /usr/local/bin/git-hooks \
+	https://raw.githubusercontent.com/alaturka/zoo/master/hooks/git-hooks
+chmod +x /usr/local/bin/git-hooks
+
 [[ -z $(echo etc/git/*) ]] || cp -a etc/git/* /etc/
+
+git-hooks --install
--- lib/scripts/operator/git_test.sh
+++ lib/scripts/operator/git_test.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 set -euo pipefail; [[ -z ${TRACE:-} ]] || set -x
+
+die() {
+	echo >&2 "$@"
+	exit 1
+}
+
+hooks_path=$(git config --system core.hooksPath) || die "git config command failed"
+[[ $hooks_path = /etc/git/hooks ]] || die "Unexpected hooks path: $hooks_path"
+
+# TODO: extra tests
