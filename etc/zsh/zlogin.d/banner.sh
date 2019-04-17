bd6dfc38bfcd3274e7628a6ca2ec4fc51bebfc05
Recai Oktaş <roktas@gmail.com>
2019-04-17 13:28:58 +0300

    İsim değişikliği s/what/banner/

--- etc/zsh/zlogin.d/banner.sh
+++ etc/zsh/zlogin.d/banner.sh
@@ -1,7 +1,7 @@
 # Print a banner
 if [[ -o interactive ]]; then
-	if type what &>/dev/null; then
-		what
+	if type banner &>/dev/null; then
+		banner
 	fi
 	# shellcheck disable=1003
 	[[ -z ${TMUX:-} ]] || printf '\033khoşgeldin\033\\'
