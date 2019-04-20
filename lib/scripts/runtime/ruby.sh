8d3eff83138e88cdccbcef6ad3cdaebe505d2a0f
Recai Oktaş <roktas@gmail.com>
2019-04-20 13:24:29 +0300

    runtime/ruby: Ruby kurulumunu basitleştir
    - Sistem Ruby kurulumunu tamamen terkediyoruz (hatta kuruluysa
      kaldırabiliyoruz)
    
    - Kurulacak Ruby sürümünü açık şekilde veriyoruz

--- lib/scripts/runtime/ruby.sh
+++ lib/scripts/runtime/ruby.sh
@@ -6,14 +6,8 @@ set -euo pipefail; [[ -z ${TRACE:-} ]] || set -x
 
 export DEBIAN_FRONTEND=noninteractive
 
-ruby_use_suites=${ruby_use_suites:-stable}
-ruby_use_experimental=${ruby_use_experimental:-}
+ruby_install_versions=${ruby_install_versions:-2.6.3}
 ruby_keep_system_ruby=${ruby_keep_system_ruby:-}
-
-fix() {
-	[[ ! -f "$1" ]] || sed -i '/BEGIN FIX/,/END FIX/d' "$1"
-	{ echo "# BEGIN FIX"; cat; echo "# END FIX"; } >>"$1"
-}
 
 system_ruby_packages=(
 	ruby
@@ -21,36 +15,21 @@ system_ruby_packages=(
 	rake
 )
 
-if [[ -n ${ruby_use_suites:-} ]]; then
-	if ! command -v rubian &>/dev/null; then
-		curl -fsSL "https://raw.githubusercontent.com/omu/rubian/master/rubian" >/usr/local/bin/rubian
-		chmod +x /usr/local/bin/rubian
-	fi
-
-	# Remove previously installed bundler gem to minimize problem surface
-	if command -v gem &>/dev/null && gem list -i bundler &>/dev/null; then
-		gem uninstall bundler
-	fi
-
-	# shellcheck disable=2086
-	rubian install $ruby_use_suites
-
-	# Purge system Ruby
-	if [[ -z ${ruby_keep_system_ruby:-} ]] && [[ -x /usr/bin/ruby ]]; then
-		apt-get -y --auto-remove purge "${system_ruby_packages[@]}" ruby-bundler
-		rubian relink
-	fi
-else
-	if [[ -n ${ruby_use_experimental:-} ]] && apt-cache policy ruby | grep -q experimental/main; then
-		apt-get -t experimental -y install --no-install-recommends "${system_ruby_packages[@]}"
-	else
-		apt-get -y install --no-install-recommends "${system_ruby_packages[@]}"
-	fi
-
-	# No documents while installing Ruby gems
-	fix /etc/gemrc <<-EOF
-		gem: --no-document
-	EOF
-
-	gem install bundler
+if ! command -v rubian &>/dev/null; then
+	curl -fsSL "https://raw.githubusercontent.com/omu/rubian/master/rubian" >/usr/local/bin/rubian
+	chmod +x /usr/local/bin/rubian
 fi
+
+# Remove previously installed bundler gem to minimize problem surface
+if command -v gem &>/dev/null && gem list -i bundler &>/dev/null; then
+	gem uninstall bundler
+fi
+
+# Purge system Ruby
+if [[ -z ${ruby_keep_system_ruby:-} ]] && [[ -x /usr/bin/ruby ]]; then
+	apt-get -y --auto-remove purge "${system_ruby_packages[@]}" ruby-bundler
+	rubian relink
+fi
+
+# shellcheck disable=2086
+rubian install $ruby_install_versions
