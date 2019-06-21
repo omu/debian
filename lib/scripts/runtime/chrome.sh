94bacbb40eaddcb19d8fb8d45a76072b56fb4ab6
Recai Oktaş <roktas@gmail.com>
2019-06-22 00:16:15 +0300

    runtime/chrome: kurulumu düzelt
    Upstream'den kurulduğunda (yani Google-Chrome) ChromeDriver da ayrı bir
    şekilde kurulmalı.  Aksi halde Debian deposundaki ChromeDriver paketi
    kurulursa Debian Chromium da bağımlılıklarda geliyor.

--- lib/scripts/runtime/chrome.sh
+++ lib/scripts/runtime/chrome.sh
@@ -7,6 +7,15 @@ set -euo pipefail; [[ -z ${TRACE:-} ]] || set -x
 export DEBIAN_FRONTEND=noninteractive
 
 if [[ -n ${chrome_install_upstream:-} ]]; then
+	latest_driver_release=$(
+		curl -fsSL https://chromedriver.storage.googleapis.com/LATEST_RELEASE
+	)
+
+	if [[ -z $latest_driver_release ]]; then
+		echo >&2 "Couldn't determine latest Google Chrome Driver release"
+		exit 1
+	fi
+
 	curl -fsSL https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 	echo 'deb http://dl.google.com/linux/chrome/deb/ stable main' >>/etc/apt/sources.list.d/chrome.list
 	apt-get -y update
@@ -14,6 +23,11 @@ if [[ -n ${chrome_install_upstream:-} ]]; then
 	apt-get -y install --no-install-recommends google-chrome-stable
 	# shellcheck disable=2016
 	sed -i 's+"$HERE/chrome"+"$HERE/chrome" --no-sandbox+g' /opt/google/chrome/google-chrome
-fi
 
-apt-get -y update && apt-get -y install --no-install-recommends chromedriver
+	apt-get -y install --no-install-recommends libarchive-tools # for bsdtar
+
+	curl -fsSL https://chromedriver.storage.googleapis.com/"$latest_driver_release"/chromedriver_linux64.zip |
+	bsdtar -C /usr/local/bin -o -xvf- - && chmod +x /usr/local/bin/*chrome*
+else
+	apt-get -y update && apt-get -y install --no-install-recommends chromedriver
+fi
